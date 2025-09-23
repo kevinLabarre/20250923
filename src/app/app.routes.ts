b5720e1d8e9d09fe163330f2ba25f9979fb05896
klabarre <klabarre@dawan.fr>
2025-09-23 16:08:41 +0200

    Creation de nos 1eres routes, ajout des CDN tailwind + daysiUI, creation de la navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Homepage } from '../view/homepage/homepage';
+import { NewsPage } from '../view/news-page/news-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: Homepage, title: "Accueil" },
+  { path: "actualite", component: NewsPage, title: "Actualités" },
+  { path: "actualite/:slug", component: NewsPage, title: "Actualités: détails" },
+];
