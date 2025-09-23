a88b9d4ec268e4f2ed196f9c9d190b38402a2862
klabarre <klabarre@dawan.fr>
2025-09-23 16:56:22 +0200

    Démo des routerLink + routerLinkActive et routerLinkActiveOptions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Homepage } from '../view/homepage/homepage';
 import { NewsPage } from '../view/news-page/news-page';
+import { AccountsPage } from '../view/accounts-page/accounts-page';
 
 export const routes: Routes = [
   { path: "", component: Homepage, title: "Accueil" },
   { path: "actualite", component: NewsPage, title: "Actualités" },
   { path: "actualite/:slug", component: NewsPage, title: "Actualités: détails" },
+  { path: "compte", component: AccountsPage, title: "Les comptes" },
 ];
---
+++ src/view/accounts-page/accounts-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accounts-page',
+  imports: [],
+  templateUrl: './accounts-page.html',
+  styleUrl: './accounts-page.css'
+})
+export class AccountsPage {
+
+}
