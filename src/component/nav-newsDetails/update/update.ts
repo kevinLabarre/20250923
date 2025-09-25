0f3b24743df75ea7449c47096e051d8d5a41e733
klabarre <klabarre@dawan.fr>
2025-09-25 16:26:58 +0200

    exemple partage de données via un service _ 2

--- src/component/nav-newsDetails/update/update.ts
+++ src/component/nav-newsDetails/update/update.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ShareDataService } from '../../../service/shareData/share-data-service';
 
 @Component({
   selector: 'app-update',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './update.html',
   styleUrl: './update.css'
 })
-export class Update {
+export class Update implements OnInit, OnDestroy {
+
+  constructor(private shareService: ShareDataService) { }
+
+  private subscription = new Subscription();
+
+  ngOnInit(): void {
+    this.subscription = this.shareService.newsShareByShareService$.subscribe(data => console.log("Donnée récupérée dans 'update'", data));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
